7aed389f99d5f490b606abade8c464d39903ab22
Monolus <j.cagnoncle@student.isartdigital.com>
2021-09-07 12:47:19 +0200

    TitleScreen

---
+++ FactoryPanic/Assets/_FactoryPanic/Game_Design_Prog/Scripts/Narrative/TittleScreen.cs
@@ -0,0 +1,22 @@
+///-----------------------------------------------------------------
+///   Author : Julien Cagnoncle                    
+///   Date   : 07/09/2021 11:57
+///-----------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.IsartDigital.FactoryPanic.GameDesignProg.Narrative {
+
+    public class TittleScreen : MonoBehaviour {
+        public void StartGame()
+        {
+            SceneManager.LoadScene("_FactoryPanic/Scenes/Narration_test", LoadSceneMode.Single);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
