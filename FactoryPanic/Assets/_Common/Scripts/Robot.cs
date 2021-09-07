5e8003940f0a8055dba0aeabd4c2cfdcf9317a6a
Tristan <t.francillonne@hotmail.fr>
2021-09-07 14:19:18 +0200

    drag and drop & factory(WIP) & class Robot

---
+++ FactoryPanic/Assets/_Common/Scripts/Robot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Imatricule = 4;
+    private float TimeLerp = 0;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
