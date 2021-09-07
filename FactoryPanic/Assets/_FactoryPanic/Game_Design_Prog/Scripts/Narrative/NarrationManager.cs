02c7b70349c5a914ffe4cf7616a1bfa2bd18fec8
Monolus <j.cagnoncle@student.isartdigital.com>
2021-09-07 16:29:33 +0200

    Add Manager Text

--- FactoryPanic/Assets/_FactoryPanic/Game_Design_Prog/Scripts/Narrative/NarrationManager.cs
+++ FactoryPanic/Assets/_FactoryPanic/Game_Design_Prog/Scripts/Narrative/NarrationManager.cs
@@ -103,13 +103,39 @@ namespace Com.IsartDigital.FactoryPanic.GameDesignProg.Narrative {
         public void Load1()
         {
             Open();
-            StartText(managerBox.LoadBlock(1));
+            StartText(managerBox.LoadBlock(0));
         }
 
         public void Load2()
         {
             Open();
+            StartText(managerBox.LoadBlock(1));
+        }
+
+        public void Load3()
+        {
+            Open();
             StartText(managerBox.LoadBlock(2));
+        }
+        public void Load4()
+        {
+            Open();
+            StartText(managerBox.LoadBlock(3));
+        }
+        public void Load5()
+        {
+            Open();
+            StartText(managerBox.LoadBlock(4));
+        }
+        public void Load6()
+        {
+            Open();
+            StartText(managerBox.LoadBlock(5));
+        }
+        public void Load7()
+        {
+            Open();
+            StartText(managerBox.LoadBlock(6));
         }
     }
 }
